5792d9e693328bdb024ffbc43b7fa18647730640
suhanitiwari0306 <suhanitiwari@utexas.edu>
2026-04-09 20:04:09 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <strata-chat workspace="mis372t" />;
+  return (
+    <div>
+      <h1>Test page</h1>
+      <p>If you can see this, React is working.</p>
+      <strata-chat workspace="mis372t"></strata-chat>
+    </div>
+  );
 }
 
 export default App;
