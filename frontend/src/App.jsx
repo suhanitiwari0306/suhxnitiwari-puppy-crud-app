3082e0023be4362330858b4f62fc8b7d40af5b5b
suhanitiwari0306 <suhanitiwari@utexas.edu>
2026-04-09 20:01:07 -0500

    test app auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,5 @@
-import Header from './components/Header';
-import Body from './components/Body';
-import Footer from './components/Footer';
-import './index.css';
-
 function App() {
-  return (
-    <div className="app-container">
-      <Header />
-      <Body />
-
-      {/* Chatbot */}
-      <strata-chat workspace="mis372t" />
-
-      <Footer />
-    </div>
-  );
+  return <strata-chat workspace="mis372t" />;
 }
 
 export default App;
